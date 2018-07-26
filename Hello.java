5beb7b05b77c2d1419b7795079b0fb0fd966814a
Ashwini Nagpure <ash.nagpure@gmail.com>
2018-07-26 05:51:31 -0700

    Added HelloWorld

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+		public static void main (String args[])
+		{
+		System.out.println("This is my first Java Program");
+                  for(int i=0;i<=10;i++)
+		   {
+		    System.out.println(" Hello World.....!!!"+i);
+		}
+             }
+	    }
+
